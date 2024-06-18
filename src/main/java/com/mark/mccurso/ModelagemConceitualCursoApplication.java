e4d9d9b37019881825a7f8022ffd1b5a108ddfc5
omarkfrank <omarkfrank@yahoo.com>
2024-06-18 17:17:00 -0300

    Projeto criado

---
+++ src/main/java/com/mark/mccurso/ModelagemConceitualCursoApplication.java
@@ -0,0 +1,12 @@
+package com.mark.mccurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemConceitualCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemConceitualCursoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mark/mccurso/ModelagemConceitualCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.mccurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemConceitualCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
