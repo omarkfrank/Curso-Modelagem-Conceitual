1589070e2a44a2af2b9f1753c6c3fe311e4609c5
omarkfrank <omarkfrank@yahoo.com>
2024-06-18 21:14:35 -0300

    Criado a operacao de instanciacao

--- src/main/java/com/mark/mccurso/ModelagemConceitualCursoApplication.java
+++ src/main/java/com/mark/mccurso/ModelagemConceitualCursoApplication.java
@@ -1,12 +1,31 @@
 package com.mark.mccurso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mark.mccurso.domain.Categoria;
+import com.mark.mccurso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemConceitualCursoApplication {
+public class ModelagemConceitualCursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualCursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
