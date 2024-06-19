3484fae43286922f231b3677453fac012dfc364a
omarkfrank <omarkfrank@yahoo.com>
2024-06-19 20:23:43 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mark/mccurso/domain/Produto.java
+++ src/main/java/com/mark/mccurso/domain/Produto.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,6 +26,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preço;
 
+	@JsonBackReference
 	@ManyToAny
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
